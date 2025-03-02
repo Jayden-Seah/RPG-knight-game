f326a9eabfb7f29fa2b8596d2c2771bf5b02aa0b
Jayden-Seah <jdseahgming2409@gmail.com>
2025-03-02 20:50:02 +0800

    Main Set up and libraries added

---
+++ main.py
@@ -0,0 +1,15 @@
+#Libraries
+
+#From File " SimplePygame.py"
+from SimplePyGame import Game, Sprite, pygame
+
+#Start
+print("Starting...")
+
+#Set up
+game = Game("Simple RPG")
+game.background_color = (128, 255, 96)
+
+
+#Running ... 
+game.run()
